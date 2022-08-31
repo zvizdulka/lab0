eb660bb5549f929e1a24985b7953b3ff5b97e05e
Ulia <bolotskihh52@gmail.com>
2022-08-31 23:46:33 +0300

    .

--- main.cpp
+++ main.cpp
@@ -16,11 +16,19 @@ bool isEven1(int value){
     return false;
 }
 
+bool isEven3(int value){
+    return (value & 1) == 0;
+}
+
+
+
 int main(int argc, const char * argv[]) {
     int a;
     cout << "Enter a number: " << endl;
     cin >> a;
     cout << isEven(a) << endl;
     cout << isEven1(a) << endl;
+    cout << isEven3(a) << endl;
+    
 }
 
